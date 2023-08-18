d69fd81384f1e474605cd310914179fd880f49e9
Vinicius Oliveira <vinicius@myriad.codes>
2023-08-18 15:01:58 -0300

    chore: adds nix devShell and norminette checks

---
+++ flake.nix
@@ -0,0 +1,56 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs";
+    ft-nix = {
+      url = "github:vinicius507/42-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    ft-nix,
+  }: let
+    allSystems = [
+      "x86_64-linux"
+      "aarch64-linux"
+    ];
+    forEachSystem = f:
+      nixpkgs.lib.genAttrs allSystems (system:
+        f {
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ft-nix.overlays.norminette];
+          };
+        });
+  in {
+    checks = forEachSystem ({pkgs}: let
+      norminetteCheck = check-name: src:
+        pkgs.stdenvNoCC.mkDerivation {
+          inherit src;
+          name = "norminette-check-${check-name}";
+          dontBuild = true;
+          doCheck = true;
+          checkPhase = with pkgs; ''
+            ${norminette}/bin/norminette $src
+          '';
+          installPhase = "touch $out"; # Derivation must build an output.
+        };
+    in {
+      norminette-check-mandatory = norminetteCheck "mandatory" ./mandatory;
+      norminette-check-bonus = norminetteCheck "bonus" ./bonus;
+    });
+    devShells = forEachSystem ({pkgs}: {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          bear
+          clang-tools_12
+          gnumake
+          llvmPackages_12.libcxxClang
+          norminette
+          valgrind
+        ];
+      };
+    });
+  };
+}
